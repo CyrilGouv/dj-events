02818e4f684a7ca0570fe989b84bfa2ec053a397
Cyril Gouv <hello@cyrilgouv.com>
2021-10-22 10:00:38 +0200

    Add getStaticProps to index

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,22 @@
 import Layout from '@/components/Layout'
+import { API_URL } from '@/config/index'
 
-export default function HomePage() {
+export default function HomePage({ events }) {
+
     return (
         <Layout>
-
-            <h1>Home</h1>
+            <h1>Upcomming Events</h1>
         </Layout>
     )
+}
+
+
+export async function getStaticProps() {
+    const res = await fetch(`${API_URL}/api/events`)
+    const events = await res.json()
+
+    return {
+        props: {events},
+        revalidate: 1
+    }
 }
