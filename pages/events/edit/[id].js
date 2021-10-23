4a1676ffcef025e40619b13b89a1ba02d592dbce
Cyril Gouv <hello@cyrilgouv.com>
2021-10-23 23:54:35 +0200

    Add Image Upload

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -7,6 +7,7 @@ import Link from 'next/link'
 import Image from "next/image";
 import Layout from "@/components/Layout"
 import Modal from "@/components/Modal"
+import ImageUpload from "@/components/ImageUpload";
 import { API_URL } from "@/config/index"
 import styles from '@/styles/Form.module.css'
 import { FaImage } from "react-icons/fa";
@@ -58,6 +59,14 @@ export default function EditEventPage({ evt }) {
     const handleInputChange = e => {
         const { name, value } = e.target
         setValues({ ...values, [name]: value })
+    }
+
+    const imageUploaded = async (e) => {
+        const res = await fetch(`${API_URL}/events/${evt.id}`)
+        const data = await res.json()
+
+        setImagePreview(data.image.formats.thumbnail.url)
+        setShowModal(false)
     }
 
     return (
@@ -127,7 +136,7 @@ export default function EditEventPage({ evt }) {
             </div>
 
             <Modal show={ showModal } onClose={() => setShowModal(false)}>
-                Image Upload
+                <ImageUpload evtId={ evt.id } imageUploaded={ imageUploaded } />
             </Modal>
         </Layout>
     )
