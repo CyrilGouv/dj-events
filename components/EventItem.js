a1cf00ff7a8d721d20de7654329a5664ee471031
Cyril Gouv <hello@cyrilgouv.com>
2021-10-23 10:51:54 +0200

    Connecting to the Strapi API

--- components/EventItem.js
+++ components/EventItem.js
@@ -1,17 +1,17 @@
 import Link from 'next/link'
-import Image from 'next/Image'
+import Image from 'next/image'
 import styles from '@/styles/EventItem.module.css'
 
 export default function EventItem({ evt }) {
     return (
         <div className={ styles.event }>
             <div className={ styles.img }>
-                <Image src={ evt.image ? evt.image : '/images/event-default.png' } width={170} height={100} />
+                <Image src={ evt.image ? evt.image.formats.thumbnail.url : '/images/event-default.png' } width={170} height={100} />
             </div>
 
             <div className={ styles.info }>
                 <span>
-                    { evt.date } at { evt.time }
+                    { new Date(evt.date).toLocaleDateString('fr-FR') } at { evt.time }
                 </span>
                 <h3>{ evt.name }</h3>
             </div>
