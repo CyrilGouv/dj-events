b78cb8f49b5199418cc81c4700ed781f94b3a97a
Cyril Gouv <hello@cyrilgouv.com>
2021-11-13 11:37:30 +0100

    Persist Logged in User

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -8,6 +8,10 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null)
     const [error, setError] = useState(null)
 
+    const router = useRouter()
+
+    useEffect(() => checkUserLoggedIn(), [])
+
     // Register user
     const register = async (user) => {
         console.log(user)
@@ -45,7 +49,16 @@ export const AuthProvider = ({ children }) => {
 
     // Check if user logged in
     const checkUserLoggedIn = async (user) => {
-        console.log('Check')
+        const res = await fetch(`${NEXT_URL}/api/user`)
+
+        const data = await res.json()
+
+        if (res.ok) {
+            setUser(data.user)
+            router.push('/account/dashboard')
+        } else {
+            setUser(null)
+        }
     }
 
     return (
